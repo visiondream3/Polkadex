59aa5dd201848d3a2279f91cbe35d88276ea4207
Gautham J <Gauthamastro@gmail.com>
2020-10-08 22:19:55 +0530

    Added Scripts to launch 4 nodes

---
+++ scripts/validator_dave.sh
@@ -0,0 +1,9 @@
+./target/release/node-polkadex \
+  --base-path /tmp/bob \
+  --chain local \
+  --dave \
+  --port 30334 \
+  --ws-port 9945 \
+  --rpc-port 9934 \
+  --validator \
+  --bootnodes /ip4/13.235.190.203/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
